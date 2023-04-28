e101fe1db53fdb519fa25a68e5bd9b2cda285127
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2023-04-28 12:46:54 -0400

    Added defaults

---
+++ templates/struct-component.rs
@@ -0,0 +1,32 @@
+use yew::{html, Component, Context, Html};
+
+pub enum Msg {
+    // component messages
+}
+
+pub struct StructComponent {
+    // component state variables
+}
+
+impl Component for StructComponent {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0, }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            // match each message, return true
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <h1>Struct Component</h1>
+            </div>
+        }
+    }
+}
